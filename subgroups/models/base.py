264fbb1ab9f378d4dc2fc2bdcca110703c24dcab
djuna <djunavm@gmail.com>
2025-05-04 10:30:10 -0400

    adding random state

--- subgroups/models/base.py
+++ subgroups/models/base.py
@@ -6,8 +6,15 @@ class SklearnClassifier(ClassifierMixin, BaseEstimator):
     """Any object that is BOTH BaseEstimator and ClassifierMixin."""
 
 @chz.chz
-class ModelFactory():
-    params: dict
+class ModelFactory(): # only methods on interfaces
+    seed: int = chz.field(default=None, doc='Seed to initialize the model factory. By default, the seed is not set so the sequence of models built by this factory is non-deterministic.')
+
+    @chz.init_property
+    def _random_state(self) -> np.random.RandomState:
+        return np.random.default_rng(self.seed)
 
     def build_model(self) -> SklearnClassifier:
         ...
+
+
+    
--- subgroups/models/classifier.py
+++ subgroups/models/classifier.py
@@ -4,6 +4,24 @@ import chz
 
 @chz.chz
 class XgbFactory(ModelFactory):
+    learning_rate: float = chz.field(default=0.1, doc='Learning rate for the model')
+    max_depth: int = chz.field(default=6, doc='Maximum depth of the tree')
+    n_estimators: int = chz.field(default=100, doc='Number of trees in the model')
+    reg_lambda: float = chz.field(default=1.0, doc='Regularization parameter for the model')
+    reg_alpha: float = chz.field(default=0.0, doc='Regularization parameter for the model')
+    subsample: float = chz.field(default=0.8, doc='Subsample ratio for the model')
+    colsample_bytree: float = chz.field(default=0.8, doc='Column subsample ratio for the model')
+    gamma: float = chz.field(default=0.0, doc='Minimum loss reduction required to make a split')
+    min_child_weight: int = chz.field(default=1, doc='Minimum sum of instance weight (hessian) needed in a child')
 
     def build_model(self) -> SklearnClassifier:
-        return XGBClassifier(**self.params)
+        return XGBClassifier(learning_rate=self.learning_rate, 
+                             max_depth=self.max_depth, 
+                             n_estimators=self.n_estimators, 
+                             reg_lambda=self.reg_lambda, 
+                             reg_alpha=self.reg_alpha, 
+                             subsample=self.subsample, 
+                             colsample_bytree=self.colsample_bytree, 
+                             gamma=self.gamma, 
+                             min_child_weight=self.min_child_weight,
+                             random_state=self._random_state)
