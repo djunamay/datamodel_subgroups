88c254af8a598df14c9ef7362fa357de401681f2
djuna <djunavm@gmail.com>
2025-07-03 08:44:55 -0400

    print n models

---
+++ subgroups/experiments/print_n_models.py
@@ -0,0 +1,16 @@
+import glob
+import numpy as np
+import chz
+
+def find_files_with_suffix(directory, suffix):
+    pattern = f"{directory}/*{suffix}"
+    return sorted(glob.glob(pattern))
+
+
+def print_n_models(path, suffix='test_accuracies.npy'):
+    files = find_files_with_suffix(path, suffix)
+    n_models = np.sum([np.sum(np.load(file)!=0) for file in files])
+    print(n_models, ' classifiers were trained.')
+
+if __name__ == "__main__":
+    chz.entrypoint(print_n_models)
