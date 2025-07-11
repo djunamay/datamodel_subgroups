94b9969ce722c2d17a81fdc5047706045c94acb1
djuna <djunavm@gmail.com>
2025-07-11 13:06:53 -0400

    fixing random state

--- subgroups/experiments/run_counterfactuals.py
+++ subgroups/experiments/run_counterfactuals.py
@@ -2,12 +2,12 @@ from ..counterfactuals.counterfactuals import CounterfactualEvaluation, Counterf
 from ..counterfactuals.datahandling import PartitionStorageBase
 import pandas as pd
 from sklearn.cluster import SpectralClustering
-
+import numpy as np
 
 def run_counterfactuals(cf_inputs, counterfactual_estimator, n_iter=100, n_clusters=2, random_state=None, shuffle_rng=None, model_rng=None):
     all_results = []
     for name, mat in cf_inputs:
-        partition_storage = PartitionStorageBase(matrix=mat, partitioner=SpectralClustering(n_clusters=n_clusters, affinity='precomputed', assign_labels='kmeans', random_state=random_state))
+        partition_storage = PartitionStorageBase(matrix=mat, partitioner=SpectralClustering(n_clusters=n_clusters, affinity='precomputed', assign_labels='kmeans', random_state=np.random.RandomState(random_state.bit_generator)))
         experiment_result = CounterfactualExperimentResults(counterfactual_estimator=counterfactual_estimator, partition_storage=partition_storage, model_rng = model_rng, shuffle_rng = shuffle_rng, n_iter=n_iter)
         results = experiment_result.results
         results['input'] = str(name)
