0a98e6f6085a42b1de997d2e782989e5d70a1033
djuna <djunavm@gmail.com>
2025-11-10 18:01:39 -0800

    adding new splitters

--- subgroups/counterfactuals/splitters.py
+++ subgroups/counterfactuals/splitters.py
@@ -5,6 +5,7 @@ import numpy as np
 from ..datastorage.results import ResultsStorage
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.manifold import SpectralEmbedding
+from sklearn.decomposition import PCA
 
 @chz.chz
 class SpectralSplitWeights(SplitFactoryInterface):
@@ -46,7 +47,76 @@ class SpectralSplitWeights(SplitFactoryInterface):
     @chz.init_property
     def split(self):
         return self._split
+
+@chz.chz
+class PCASplitWeights(SplitFactoryInterface):
+
+    experiment: Experiment
+    split_class_1: bool
+    percentage: float
+    pc : int
+
+    @chz.init_property
+    def _results(self):
+        return ResultsStorage(self.experiment)
+
+    @chz.init_property
+    def _embedding(self):
+        embedder = PCA(n_components=self.pc+1, svd_solver="auto", random_state=0)
+        return embedder.fit_transform(self._results.weights.T)[:,self.pc].reshape(-1)
+
+    @chz.init_property
+    def _labels(self):
+        if self.split_class_1:
+            return self.experiment.dataset.coarse_labels
+        else: 
+            return np.invert(self.experiment.dataset.coarse_labels)
+
+    @chz.init_property
+    def _split(self):
+        split = np.zeros(len(self._labels), dtype=bool)
+        split[self._labels] = (self._embedding)[self._labels]>np.percentile(self._embedding[self._labels], self.percentage)  
+        return split
+
+    @chz.init_property
+    def split(self):
+        return self._split
     
+
+
+@chz.chz
+class PCASplitWeights2(SplitFactoryInterface):
+
+    experiment: Experiment
+    split_class_1: bool
+    percentage: float
+    pc : int
+
+    @chz.init_property
+    def _results(self):
+        return ResultsStorage(self.experiment)
+
+    @chz.init_property
+    def _embedding(self):
+        embedder = PCA(n_components=self.pc+1, svd_solver="auto", random_state=0)
+        return embedder.fit_transform(self._results.weights.T)[:,self.pc].reshape(-1)
+
+    @chz.init_property
+    def _labels(self):
+        if self.split_class_1:
+            return self.experiment.dataset.coarse_labels
+        else: 
+            return np.invert(self.experiment.dataset.coarse_labels)
+
+    @chz.init_property
+    def _split(self):
+        split = np.zeros(len(self._labels), dtype=bool)
+        split[self._labels] = (self._embedding)[self._labels]<np.percentile(self._embedding[self._labels], self.percentage)  
+        return split
+
+    @chz.init_property
+    def split(self):
+        return self._split
 
 @chz.chz
 class SpectralSplitFeatures(SplitFactoryInterface):
@@ -89,3 +159,36 @@ class SpectralSplitFeatures(SplitFactoryInterface):
     def split(self):
         return self._split
     
+@chz.chz
+class PCASplitFeatures(SplitFactoryInterface):
+
+    experiment: Experiment
+    split_class_1: bool
+    percentage: float
+    pc: int
+
+    @chz.init_property
+    def _reduced_dim_features(self):
+        return self.experiment.dataset.features #self.experiment.dataset._reduce_dimensionality(self.experiment.dataset.untransformed_features, self.experiment.dataset.features.shape[0])
+
+
+    @chz.init_property
+    def _embedding(self):
+        return self._reduced_dim_features[:,self.pc]
+
+    @chz.init_property
+    def _labels(self):
+        if self.split_class_1:
+            return self.experiment.dataset.coarse_labels
+        else: 
+            return np.invert(self.experiment.dataset.coarse_labels)
+
+    @chz.init_property
+    def _split(self):
+        split = np.zeros(len(self._labels), dtype=bool)
+        split[self._labels] = (self._embedding)[self._labels]>np.percentile(self._embedding[self._labels], self.percentage)  
+        return split
+
+    @chz.init_property
+    def split(self):
+        return self._split
